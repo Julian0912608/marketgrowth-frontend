532dcb38bd47737bf58d8be9019d0a2022f2f17b
Julian0912608 <juligoventures@gmail.com>
2026-03-12 23:12:10 +0100

    Update index.ts

--- components/marketing/index.ts
+++ components/marketing/index.ts
@@ -1,1 +1,1 @@
-export { Demo, Pricing, Testimonials, FAQ, CTA, Footer } from './sections';
+export { ProductPerformance, MarketingInsights, GrowthOpportunities, AdCreative, PlatformBar, Pricing, Testimonials, FAQ, CTA, Footer } from './sections';
